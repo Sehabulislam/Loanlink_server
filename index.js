a6831323a90ff26637c8a21ff62c3f8cbf8d8201
Sehabulislam <shehabulislam244@gmail.com>
2025-12-10 21:46:22 +0600

    added the loans apis

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Middleware
+app.use(cors());
+app.use(express.json());
 
+// MongoDB connection
+const uri =
+  `mongodb+srv://${process.env.VITE_USER}:${process.env.VITE_PASS}@cluster0.cjsthkf.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Loan Link server is running.");
+});
+async function run() {
+  try {
+    await client.connect();
+    const loanLinkDB = client.db("loanLinkDB");
+    const loansCollection = loanLinkDB.collection("loans");
+
+    app.get("/loans", async (req, res) => {
+      const cursor = loansCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.post("/loans",async(req,res)=>{
+        const loan = req.body;
+        const result = await loansCollection.insertOne(loan);
+        res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
